3d32268a62bf04543385a83e6a66403a3eb28058
serap <serapp.oral@gmail.com>
2022-06-08 22:22:34 +0300

    load

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const input=document.getElementById("do");
-const addBtn=document.getElementById("addBtn");
-const liste=document.getElementById("liste");
+const input=document.querySelector("todo_input");
+const submitBtn=document.querySelector("todo_btn");
 
-addBtn.addEventListener('click',() =>{
-     liste.innerHTML +=
-     `<input type="checkbox" id="check" value="Bike"> 
-     <label for="check">${input.value}</label><i class="fas fa-trash"></i>`;
-     input.value="";
+submitBtn.addEventListener('click', () =>{
      
 });
-
-
