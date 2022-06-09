d03b6146df99a9ba0a43e7a3426c348b798a3a27
serap <serapp.oral@gmail.com>
2022-06-09 23:27:48 +0300

    js

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
-const input=document.querySelector("todo_input");
+const input=document.getElementById("todo_input");
 const form=document.querySelector(".todo_form");
 const submitBtn=document.getElementsByClassName("todo_btn");
+const todo_container=document.getElementById("todo_container");
+
+const starfConf =() =>{
+
+const todos=localStorage.getItem("todos");
+
+if(!todos){
+    localStorage.setItem("todos", JSON.stringify([]));
+}
+}
+
+starfConf();
+
 
 const addtoDo =(e) =>{
-     
+   e.preventDefault();
+
+ 
+   const todo ={
+       text:input.value,
+       isCompleted:false,
+
+   };
+
+   const todos=JSON.parse(localStorage.getItem("todos"));
+   todos.push(todo);
+   localStorage.setItem("todos", JSON.stringify(todos));
+   console.log(todos);
+
+   const todoDiv=document.getElementById("div");
+   todoDiv.classList.add(todo);
+   
+   const todoLeft=document.createElement("div");
+   todoLeft.classList.add("todo_left");
+
+
+   const todoText=document.createElement("span");
+   todoText.classList.add("todo_text");
+   todoText.textContent=todo.text;
+
+   todo_container.appendChild(todoDiv);
+   todoLeft.appendChild("todo_text");
 }
 
 form.addEventListener("submit", addtoDo);
+
+
+
+
+
